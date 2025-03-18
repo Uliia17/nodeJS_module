92746f3fe9f4097fbca50051f5d891ce218ee31d
Uliia17 <45678julia@gmail.com>
2025-03-18 14:11:59 +0200

    Перенесено файли з гілки hw/hw3 репозиторія clone_nodeJS до lessons/lesson4

--- src/enums/status-codes.enum.ts
+++ src/enums/status-codes.enum.ts
@@ -2,8 +2,8 @@ export enum StatusCodesEnum {
     OK = 200,
     CREATED = 201,
     NO_CONTENT = 204,
-    BED_REQUEST = 400,
+    BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
     FORBIDDEN = 403,
-    NOTFOUND = 404
+    NOTFOUND = 404,
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import express from "express";
-import * as mongoose from "mongoose";
-import config from "./configs/config";
-import {apiRouter} from "./routers/api.router";
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import { config } from "./configs/config";
+import { apiRouter } from "./routers/api.router";
+import { ApiError } from "./errors/api.error";
 
 const app = express();
 app.use(express.json());
@@ -9,29 +12,43 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", apiRouter);
 
+app.use(
+    "*",
+    (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+        const status = err.status || 500;
+        const message = err.message ?? "Something went wrong";
+        res.status(status).json({ status, message });
+    },
+);
+process.on("uncaughtException", (err) => {
+    console.log("uncaughtException", err);
+    process.exit(1);
+});
 const dbConnection = async () => {
-  let dbCon = false
+    let dbCon = false;
 
     while (!dbCon) {
-      try {
-          console.log('Connecting to DB connection...');
-          await mongoose.connect(config.MONGO_URI);
-          dbCon = true;
-          console.log('Database available!!!');
-      }catch (e) {
-          console.log('Database unavailable, wait 3 seconds!');
-          await new Promise(resolve => setTimeout(resolve, 3000));
-      }
+        try {
+            console.log("Connecting to DB...");
+            await mongoose.connect(config.MONGO_URI);
+            dbCon = true;
+            console.log("Database available!!!");
+        } catch (e) {
+            console.log("Database unavailable, wait 3 seconds");
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+        }
     }
-}
+};
 
 const start = async () => {
-  try {
-      await dbConnection();
-      app.listen(config.PORT, () =>
-          console.log(`Server started on ${config.PORT}`));
-  }catch (e) {
-      console.log(e);
-  }
-}
+    try {
+        await dbConnection();
+        app.listen(config.PORT, () => {
+            console.log(`Server listening on ${config.PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
 start();
